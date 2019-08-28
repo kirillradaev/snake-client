1526f65f24bb886f779b297382d9329b15f55c35
kirillradaev <cyrilrdv@gmail.com>
2019-08-28 22:38:43 +0000

    modified the connect fucntion and made exports

--- client.js
+++ client.js
@@ -1,9 +1,10 @@
 const net = require('net');
+const { IP, PORT } = require('./constants');
 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '192.168.88.151',
-    port: 50541
+    host: IP,
+    port: PORT
     
   });
   conn.on('data', (data) => {
@@ -12,14 +13,6 @@ const connect = function() {
   conn.on('connect', () => {
     conn.write('Name: KVR');
   });
-  // conn.on('connect', () => {
-  //   conn.write('Move: up');
-  // });
-  // conn.on('connect', () => {
-  //   setInterval(function(){
-  //   conn.write('Move: left')}, 50);
-  // });
-
   conn.setEncoding('utf8'); 
   conn.on('data', (data) => {
     console.log(data);
