3a56abf87a1f55f3998a7aec1bbc4fd1cb6ee9c3
kirillradaev <cyrilrdv@gmail.com>
2019-08-28 20:02:25 +0000

    added new files and changes previous files

--- client.js
+++ client.js
@@ -1,22 +1,23 @@
 const net = require('net');
 
-/**
- * Establishes connection with the game server
- */
 const connect = function() {
   const conn = net.createConnection({ 
     host: '192.168.88.151',
     port: 50541
     
   });
-  // interpret incoming data as text
+  conn.on('data', (data) => {
+    console.log('Successfully connected to game server')
+  });
+  conn.write('Name: GOD')
   conn.setEncoding('utf8'); 
   conn.on('data', (data) => {
     console.log(data);
   });
   return conn;
-  
 }
 
-console.log('Connecting ...');
-connect();
+// console.log('Connecting ...');
+// connect();
+
+module.exports = connect;
